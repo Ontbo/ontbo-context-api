b786a66aa7bb2bc54ca37776c90254653850e65a
Stéphane AUBRY <stephane.aubry@ontbo.com>
2025-09-29 17:44:50 +0200

    Documentation on the upload_json_profile example.

--- samples/upload_json_profile/01_create_profile.py
+++ samples/upload_json_profile/01_create_profile.py
@@ -1,4 +1,3 @@
-from config import config
 from ontbo import Ontbo, SceneMessage
 from pathlib import Path
 import json
@@ -6,19 +5,26 @@ import json
 
 if __name__ == "__main__":
 
+    # Script params.
+    API_KEY="<YOUR_API_KEY>"
     PROFILE_NAME = "youssef"
     DATA_PATH = "datasets/youssef_50_sessions"
+    
+    ontbo = Ontbo(API_KEY)
 
-    ontbo = Ontbo(token=config["API_KEY"], base_url=config["BASE_URL"])
+    # (optional) If a profile with the same ID already exists, we delete it.
+    if PROFILE_NAME in ontbo.profile_ids:
+        ontbo.delete_profile(PROFILE_NAME)
 
+    # Create the new profile.
     profile=ontbo.create_profile(PROFILE_NAME)
 
-    print(f"Profile created profile ID is {profile.id}")
+    # Check the ID of the created profile.
+    print(f"Profile created profile ID is {profile.id}.")
     print(f"Use this ID to query the profile in the query script")
 
-
+    # Get the list of JSON files to upload.
     data_dir = Path(__file__).resolve().parent / DATA_PATH
-
     json_files = list(data_dir.glob("*.json"))
 
     chat_count = len(json_files)
@@ -28,15 +34,25 @@ if __name__ == "__main__":
         current_chat += 1
         print(f"Uploading chat {current_chat}/{chat_count}")
 
+        # Load JSON data, each message is a dict, with the following structure:
+        # {
+        #   "content":content,
+        #   "role":role,
+        #   "timestamp":timestamp,
+        # }
         with open(file) as f:
             conversation_data = json.loads(f.read())
 
+        # Each dict in the conversation is conerted in a SceneMessage.
         mesages = [SceneMessage(
                 content=message["content"],
                 role=message["role"],
                 timestamp=message["timestamp"]
             ) for message in conversation_data]
         
+        # Create the scene and upload the messages.
+        # If you want to be sure that the profile computation is done when the 
+        # script ends, set wait_for_result to true.
         scene=profile.create_scene("scene")
         scene.add_messages(mesages, update_now=True, wait_for_result=False)
 
--- samples/upload_json_profile/02_query_profile.py
+++ samples/upload_json_profile/02_query_profile.py
@@ -1,17 +1,15 @@
-from config import config
 from ontbo import Ontbo, SceneMessage
 from pathlib import Path
 import json
 
 
 if __name__ == "__main__":
+    # The ID of your newly created profile. 
+    API_KEY="<YOUR_API_KEY>"
+    PROFILE_ID = "youssef"
 
-    PROFILE_NAME = "youssef_0"
-
-    ontbo = Ontbo(token=config["API_KEY"], base_url=config["BASE_URL"])
-
-    profile=ontbo.profile(PROFILE_NAME)
-
+    ontbo = Ontbo(API_KEY)
+    profile=ontbo.profile(PROFILE_ID)
     print(profile.query_facts("What is the user's job?"))
 
 
